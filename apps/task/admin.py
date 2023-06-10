6f01e0028a9bb308e539d25ab8ab461bc61297e7
Shoki Nakano <nanaedashokudai@gmail.com>
2023-06-11 01:24:49 +0900

    11 タスクの項目の追加と追加の仕組みづくり (#20)
    upd

--- apps/task/admin.py
+++ apps/task/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Task
+from .models import Roadmap, Milestone, Task
 
+admin.site.register(Roadmap)
+admin.site.register(Milestone)
 admin.site.register(Task)
