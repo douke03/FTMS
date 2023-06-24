b292724f3b16aab39c05d93c68effd1cbf17d81f
Shoki Nakano <nanaedashokudai@gmail.com>
2023-06-24 23:58:17 +0900

    On branch 25-軽微な不具合修正 Your branch is up to date with 'origin/25-軽微な不具合修正'.
    Changes to be committed:
    	modified:   apps/core/migrations/0001_initial.py
    	modified:   apps/core/models.py
    	modified:   apps/task/migrations/0001_initial.py
    	modified:   apps/task/models.py
    	modified:   locale/ja/LC_MESSAGES/django.po

--- apps/task/models.py
+++ apps/task/models.py
@@ -48,42 +48,27 @@ class Task(CommonItem):
         verbose_name = _("task")
         verbose_name_plural = _("tasks")
 
-    NOT_READY = "NOT_READY"
-    READY = "READY"
-    DOING = "DOING"
-    DONE = "DONE"
-    STATUS_CHOICES = [
-        (NOT_READY, "Not Ready"),
-        (READY, "Ready"),
-        (DOING, "Doing"),
-        (DONE, "Done"),
-    ]
-    MAN_HOUR = [
-        (1, 1),
-        (2, 2),
-        (3, 3),
-        (5, 5),
-        (8, 8),
-    ]
+    class Status(models.TextChoices):
+        NOT_READY = "NOT_READY", _("Not Ready")
+        READY = "READY", _("Ready")
+        DOING = "DOING", _("Doing")
+        DONE = "DONE", _("Done")
+
     number = models.IntegerField(editable=False, verbose_name=_("#"))
     subject = models.CharField(max_length=255, verbose_name=_("subject"))
     description = models.TextField(blank=True, null=True, verbose_name=_("description"))
-    owner = models.ForeignKey(
-        Team,
-        on_delete=models.PROTECT,
-        verbose_name=_("owner"),
-    )
+    owner = models.ForeignKey(Team, on_delete=models.PROTECT, verbose_name=_("owner"))
     status = models.CharField(
         max_length=40,
-        choices=STATUS_CHOICES,
-        default=NOT_READY,
+        choices=Status.choices,
+        default=Status.NOT_READY,
         verbose_name=_("status"),
     )
     due_date = models.DateField(blank=True, null=True, verbose_name=_("due date"))
     start_date = models.DateField(blank=True, null=True, verbose_name=_("start date"))
     end_date = models.DateField(blank=True, null=True, verbose_name=_("end date"))
     estimated_man_hour = models.IntegerField(
-        choices=MAN_HOUR,
+        choices=[(1, 1), (2, 2), (3, 3), (5, 5), (8, 8)],
         default=1,
         verbose_name=_("estimated man hour"),
     )
@@ -109,8 +94,6 @@ class Task(CommonItem):
         return self.subject
 
     def save(self, *args, **kwargs):
-        self.number = get_next_value("model_task_seq")
-        if self.milestone:
-            if self.milestone.roadmap:
-                self.roadmap = self.milestone.roadmap
+        if not self.number:
+            self.number = get_next_value("model_task_seq")
         super().save(*args, **kwargs)
