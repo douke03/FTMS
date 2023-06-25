8afbc7b4b51d404cbf1cdb5238488b5f5c5e1d33
Shoki Nakano <nanaedashokudai@gmail.com>
2023-06-26 03:53:34 +0900

    動作確認 (#31)
    close #30

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-# from django.conf.urls import include, url
+from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = i18n_patterns(
-    path("admin/", admin.site.urls), prefix_default_language=False
+    path("admin/", admin.site.urls),
+    path("test/", include("apps.core.urls")),
+    prefix_default_language=False,
 )
